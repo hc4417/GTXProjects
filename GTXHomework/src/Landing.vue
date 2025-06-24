5ef0f0520444c89c59eaddb048b3ad3e935f62f7
Heather <hc4417@stern.nyu.edu>
2025-06-24 10:54:21 -0400

    added visibility toggle

--- GTXHomework/src/Landing.vue
+++ GTXHomework/src/Landing.vue
@@ -8,14 +8,18 @@
     class = "fullLength" 
     placeholder = "Enter your username"
     /> 
-
+    <div>
     Password <br>
     <input 
+      :type = "password"
       v-model = "form.pswrd" 
       class = "fullLength" 
       placeholder = "Enter your password" 
-    />    
+    /> 
 
+    <button @click = "toggleVisibility">{{visibility}}</button>   
+    </div>
+    
     <div class = "buttonContainer">
       <button @click="goToForm">Login</button>
     </div> 
@@ -26,12 +30,24 @@
 <script>
 export default {
     data(){
+    const visibilityStatus= ['show', 'hide']
+    const passwordType = ["password", "text"]
+    const visPos = 0
+    
     return{
       form: {
         username:'',
         pswrd:'',
       },
+
+      visibilityStatus,
+      passwordType,
+      visPos,
+      visibility: visibilityStatus[visPos],
+      password: passwordType[visPos]
+      
     }
+
   },
   methods:{
     goToForm(){
@@ -40,8 +56,16 @@ export default {
         this.form.pswrd.trim() === 'ABC123'
       ) {
         this.$router.push('/form')
+      } else{
+        alert("Wrong username or password.")
       }
     },
+
+    toggleVisibility(){
+      this.visPos = (this.visPos + 1) % 2;
+      this.visibility = this.visibilityStatus[this.visPos]
+      this.password= this.passwordType[this.visPos]
+    }
   }      
 };
 
