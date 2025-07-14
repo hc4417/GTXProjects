e0de2a3c4df25d32f7166c76c6b734b61c9687c2
Heather <hc4417@stern.nyu.edu>
2025-07-14 16:24:34 -0400

    formatting

--- GTXHomework/src/Form.vue
+++ GTXHomework/src/Form.vue
@@ -1,151 +1,151 @@
 <script setup>
-import { computed } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import {useProfilesStore} from '@/store'
+import { computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { useProfilesStore } from "@/store";
 
-const router = useRouter()
-const route = useRoute()
-
+const router = useRouter();
+const route = useRoute();
 </script>
 
 <!-- form -->
-<template> 
-  <div class = "backForthContainer">
-    <button class = "backForthButton" @click = "BackToLanding">Back to Login</button> 
-
+<template>
+  <div class="backForthContainer">
+    <button class="backForthButton" @click="BackToLanding">
+      Back to Login
+    </button>
   </div>
 
-  <div class = "form" :class="{ shake: shakeForm }">
-
-    First Name <br>
-    <input 
-    v-model = "form.firstName" 
-    class = "fullLength" 
-    placeholder = "First Name"
-    /> 
-    <small>{{form.firstName.length}}/50 characters</small>
-
-    Last Name <br>
-    <input 
-      v-model = "form.lastName" 
-      class = "fullLength" 
-      placeholder = "Last Name" 
-      maxLength = "50"
+  <div class="form" :class="{ shake: shakeForm }">
+    First Name <br />
+    <input
+      v-model="form.firstName"
+      class="fullLength"
+      placeholder="First Name"
     />
-    <small>{{form.lastName.length}}/50 characters</small>
-     
+    <small>{{ form.firstName.length }}/50 characters</small>
 
-    Hometown <br>
-    <input v-model = "form.hometown" class = "fullLength" placeholder = "Hometown"/>
-    <small>{{form.hometown.length}}/100 characters</small>
+    Last Name <br />
+    <input
+      v-model="form.lastName"
+      class="fullLength"
+      placeholder="Last Name"
+      maxLength="50"
+    />
+    <small>{{ form.lastName.length }}/50 characters</small>
 
-    Current City <br>
-    <input v-model = "form.currentCity" class = "fullLength" placeholder = "Current City"/>
-    <small>{{form.currentCity.length}}/100 characters</small>
-    
-    <input type = "checkbox" id = "checkbox" v-model = "checked"/>
-    <label for = "checkbox"> Dual Citizenship</label> 
+    Hometown <br />
+    <input v-model="form.hometown" class="fullLength" placeholder="Hometown" />
+    <small>{{ form.hometown.length }}/100 characters</small>
 
-    <div class = "buttonContainer">
-    <button @click = "Cleared">Clear</button>
-    <button type="button" @click = "Submitted">Submit</button> <br><br>
-  </div>
+    Current City <br />
+    <input
+      v-model="form.currentCity"
+      class="fullLength"
+      placeholder="Current City"
+    />
+    <small>{{ form.currentCity.length }}/100 characters</small>
 
-    <div class = "results">
+    <input type="checkbox" id="checkbox" v-model="checked" />
+    <label for="checkbox"> Dual Citizenship</label>
 
-      Result: 
-        <!-- Version 1
+    <div class="buttonContainer">
+      <button @click="Cleared">Clear</button>
+      <button type="button" @click="Submitted">Submit</button> <br /><br />
+    </div>
+
+    <div class="results">
+      Result:
+      <!-- Version 1
         <div v-if = "submitted">
           
           <p>Hi, I am {{form.firstName}} {{form.lastName}}. I am originally from {{form.hometown}}, currently residing in {{form.currentCity}}.</p>
         </div>
         -->
-        
-        <!-- Version 2-->
-        <div :class = "{blue: !checked, orange: checked}">
-        <p>Hi, I am {{form.firstName}} {{form.lastName}}. I am originally from {{form.hometown}}, currently residing in {{form.currentCity}}.</p>
-        </div>
 
+      <!-- Version 2-->
+      <div :class="{ blue: !checked, orange: checked }">
+        <p>
+          Hi, I am {{ form.firstName }} {{ form.lastName }}. I am originally
+          from {{ form.hometown }}, currently residing in
+          {{ form.currentCity }}.
+        </p>
+      </div>
     </div>
 
-    <br>
-    Favorite Foods: 
-      <div v-if = "submitted">
-        <ol>
-          <li v-for = "(food, index) in favoriteFoods" :key = "index">{{food}}</li>
-        </ol>
-      </div>
-
+    <br />
+    Favorite Foods:
+    <div v-if="submitted">
+      <ol>
+        <li v-for="(food, index) in favoriteFoods" :key="index">{{ food }}</li>
+      </ol>
+    </div>
   </div>
-
 </template>
 
 <!--Data Retrieval + Button Method-->
 <script>
 export default {
-  data(){
-    return{
+  data() {
+    return {
       form: {
-        firstName:'',
-        lastName:'',
-        hometown:'',
-        currentCity:''
+        firstName: "",
+        lastName: "",
+        hometown: "",
+        currentCity: "",
       },
 
       checked: false,
-      submitted:false,
+      submitted: false,
       shakeForm: false,
 
-      favoriteFoods: ['Tofu Stew', 'Salmon', 'Tom Yum']
-    }
+      favoriteFoods: ["Tofu Stew", "Salmon", "Tom Yum"],
+    };
   },
-  created(){
-    this.store = useProfilesStore()
+  created() {
+    this.store = useProfilesStore();
   },
   computed: {
     userId() {
-      return this.store.userId
-    }
+      return this.store.userId;
+    },
   },
-  
-  methods:{
 
-    Submitted(){
+  methods: {
+    Submitted() {
       if (
-        this.form.firstName.trim() === '' ||
-        this.form.lastName.trim() === '' ||
-        this.form.hometown.trim() === '' ||
-        this.form.currentCity.trim() === ''
+        this.form.firstName.trim() === "" ||
+        this.form.lastName.trim() === "" ||
+        this.form.hometown.trim() === "" ||
+        this.form.currentCity.trim() === ""
       ) {
         alert("Please fill out all fields before submitting.");
         return;
       }
 
-      this.submitted = true
-      const profile= {
-        fullName :this.form.firstName + " " + this.form.lastName,
-        origin : this.form.hometown,
+      this.submitted = true;
+      const profile = {
+        fullName: this.form.firstName + " " + this.form.lastName,
+        origin: this.form.hometown,
         currentCity: this.form.currentCity,
-        dualCitizen: this.checked
-      }
-      this.store.saveProfile(this.store.userId, profile)
-      this.$router.push('/profile')
+        dualCitizen: this.checked,
+      };
+      this.store.saveProfile(this.store.userId, profile);
+      this.$router.push("/profile");
     },
 
-    Cleared(){
-      this.form.firstName ='';
-      this.form.lastName ='';
-      this.form.hometown ='';
-      this.form.currentCity ='';
+    Cleared() {
+      this.form.firstName = "";
+      this.form.lastName = "";
+      this.form.hometown = "";
+      this.form.currentCity = "";
       this.checked = false;
       this.submitted = false;
     },
-    BackToLanding(){
-      this.$router.push('/')
+    BackToLanding() {
+      this.$router.push("/");
     },
-  }
-}
-
+  },
+};
 </script>
 
 
@@ -161,9 +161,5 @@ header {
 }
 
 @media (min-width: 1024px) {
-  
 }
-
-  
-  
 </style>
